6f08c229d1d210aa15b47763e138a540d925f05c
zitoyeo <wlsgh010203@gmail.com>
2022-02-17 01:28:50 +0900

    made Hero Section

--- src/Sections/Hero/index.js
+++ src/Sections/Hero/index.js
@@ -1,23 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Pink from "../../assets/blobPink.png";
 import White from "../../assets/blobWhite.png";
 import Purple from "../../assets/blobPurple.png";
 import Arrow from "../../assets/Arrow Right.svg";
+import Mobile from "../../assets/mobile.svg";
 
-const HerosSection = styled.div`
+const move = keyframes`
+0% { transform: translateY(-5px)  }
+    50% { transform: translateY(10px) }
+    100% { transform: translateY(-5px) }
+`;
+
+const MobileSvg = styled.img`
+  max-width: 100%;
+  width: calc(30% + 20vw);
+  z-index: 7;
+  height: auto;
+  animation: ${move} 2.5s ease infinite;
+`;
+
+const HomeSection = styled.section`
   width: 100vw;
   height: 45vw;
   background-color: #0a0b10;
-  color: var(--white);
   display: flex;
-  justify-content: centers;
+  justify-content: center;
   position: relative;
+  @media only Screen and (max-width: 48em) {
+    height: 70vw;
+    display: block;
+  }
+  @media only Screen and (max-width: 420px) {
+    height: auto;
+    padding-bottom: 2rem;
+  }
 `;
 
 const Blobs = styled.div`
   width: 100%;
   position: absolute;
   right: 0;
+  @media only Screen and (max-width: 48em) {
+    opacity: 0.5;
+  }
 `;
 
 const PinkBlob = styled.div`
@@ -27,35 +52,107 @@ const PinkBlob = styled.div`
   top: calc(5rem + 5vw);
   z-index: 6;
 `;
-const WhiteBlob = styled.div`
-  width: calc(20% + 20vw);
-  position: absolute;
-  right: calc(3.5rem + 3.5vw);
-  top: calc(2rem +2vw);
-  z-index: 5;
-`;
 const PurpleBlob = styled.div`
   width: calc(10% + 10vw);
   position: absolute;
   right: 0;
 `;
-
+const WhiteBlob = styled.div`
+  width: calc(20% + 20vw);
+  position: absolute;
+  right: calc(3.5rem + 3.5vw);
+  top: calc(2rem + 2vw);
+  z-index: 5;
+`;
 const MainContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 70vw;
+  @media only Screen and (max-width: 48em) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+  }
 `;
-const LeftBlock = styled.div``;
-const Topic = styled.div``;
-const Circle = styled.span``;
-const Title = styled.h1``;
-const SubText = styled.h5``;
-const CTA = styled.button``;
+
+const Lb = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 50%;
+  line-height: 1.5;
+  color: var(--white);
+  position: relative;
+  z-index: 15;
+  @media only Screen and (max-width: 48em) {
+    width: 80%;
+    text-align: center;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: calc(2.5rem + 2.5vw);
+    filter: drop-shadow(2px 4px 6px black);
+  }
+  @media only Screen and (max-width: 40em) {
+    filter: none;
+  }
+`;
+
+const Topic = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--nav);
+  font-weight: 700;
+  font-size: calc(0.4rem + 0.4vw);
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+`;
+const Circle = styled.span`
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  background-color: var(--purple);
+  margin-right: 0.5rem;
+`;
+const Title = styled.h1`
+  font-size: calc(2rem +1vw);
+  line-height: 1.2;
+  padding: 0.5rem 0;
+`;
+const SubText = styled.h5`
+  font-size: calc(0.5rem + 0.5vw);
+  color: var (--nav2);
+`;
+const CTA = styled.button`
+  padding: 0.5rem 1rem;
+  margin-top: 1rem;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: calc(0.5rem + 0.5vw);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+
+  img {
+    width: 1.5rem;
+  }
+`;
 
 const HeroSection = () => {
   return (
-    <HerosSection>
+    <HomeSection>
       <Blobs>
         <PinkBlob>
           <img src={Pink} alt="Pink Blob" width="400" height="400" />
@@ -68,7 +165,7 @@ const HeroSection = () => {
         </PurpleBlob>
       </Blobs>
       <MainContent>
-        <LeftBlock>
+        <Lb id="leftBlock">
           <Topic>
             <Circle /> <span>We Build Web</span>
           </Topic>
@@ -77,12 +174,13 @@ const HeroSection = () => {
             we help fast growing companies build award winning websites
           </SubText>
           <CTA>
-            Get in touch &nbsp{" "}
-            <img src={Arrow} alt="CTA" width="400" height="400" />
+            Get in touch &nbsp;
+            <img src={Arrow} alt="cta" width="100" height="100" />
           </CTA>
-        </LeftBlock>
+        </Lb>
+        <MobileSvg src={Mobile} alt="Mobile Svg" width="400" height="400" />
       </MainContent>
-    </HerosSection>
+    </HomeSection>
   );
 };
 
