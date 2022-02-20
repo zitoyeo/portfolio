ad6e55646d8e0136f8d945942ae849efba7e2c5a
zitoyeo <wlsgh010203@gmail.com>
2022-02-20 22:14:21 +0900

    About Page Development

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -3,14 +3,13 @@ import hand from "../../assets/hand.svg";
 import rocket from "../../assets/rocket image.png";
 import human from "../../assets/human.svg";
 import styled, { keyframes } from "styled-components";
-
 const move = keyframes`
-    0% { transform: translateY(-5px)}
-    50% { transform: translateY(10px) translateX(10px)}
-    100% { transform: translateY(-5px)}
+0% { transform: translateY(-5px)         }
+    50% { transform: translateY(10px) translateX(10px)        }
+    100% { transform: translateY(-5px)         }
 `;
 
-const AboutSection = styled.div`
+const AboutSection = styled.section`
   width: 100vw;
   position: relative;
   display: flex;
@@ -20,14 +19,17 @@ const AboutSection = styled.div`
 `;
 const Waves = styled.img`
   width: 100%;
+  height: auto;
   position: absolute;
   top: -1rem;
 `;
-
 const Hand = styled.div`
   position: absolute;
   bottom: -1rem;
   right: 0;
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
 `;
 
 const Main = styled.div`
@@ -36,43 +38,62 @@ const Main = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  @media only Screen and (max-width: 64em) {
+    margin: 0 calc(5rem + 5vw);
+    margin-top: 10rem;
+  }
+  @media only Screen and (max-width: 40em) {
+    align-items: center;
+    margin: 3rem calc(3rem + 3vw);
+  }
 `;
+
 const Title = styled.h1`
-  display: inline-block;
   font-size: 2rem;
+  display: inline-block;
 `;
+
 const CurvedLine = styled.div`
   width: 7rem;
   height: 2rem;
-  border: 5px solid var(--purple);
+  border: solid 5px var(--purple);
   border-color: var(--purple) transparent transparent transparent;
-  border-radius: 150% 60px 70px 0 0;
+  border-radius: 150%/60px 70px 0 0;
 `;
+
 const Content = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media only Screen and (max-width: 40em) {
+    flex-direction: column;
+  }
 `;
+
 const Rocket = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-content: center;
   width: 40%;
   padding-bottom: 5rem;
   animation: ${move} 2.5s ease infinite;
+  @media only Screen and (max-width: 40em) {
+    width: 50vw;
+    padding-bottom: 0;
+  }
 `;
-const AboutText = styled.div`
-  position: relative;
-  width: 50%;
-`;
+
 const Human = styled.div`
   width: 50%;
   position: absolute;
   right: 0;
   bottom: 100%;
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
 `;
 const Text = styled.h4`
-  font-size: calc(0.5rem+ 1vw);
+  font-size: calc(0.5rem + 1vw);
   line-height: 1.5;
   color: var(--nav2);
 `;
@@ -81,19 +102,25 @@ const Circle = styled.span`
   width: 1rem;
   height: 1rem;
   border-radius: 50%;
-  background-color: var(--black);
-  margin-right: 0;
+  background-color: black;
+  margin-right: 0.5rem;
   margin-top: 1rem;
 `;
+const AboutText = styled.div`
+  width: 50%;
+  position: relative;
+  @media only Screen and (max-width: 40em) {
+    width: 100%;
+  }
+`;
+
 const About = () => {
   return (
-    <AboutSection>
-      <Waves src={waves} alt="waves" width="400" height="400" />
-
+    <AboutSection id="about">
+      <Waves src={waves} alt="" />
       <Hand>
-        <img src={hand} alt="Hand" width="400" height="400" />
+        <img src={hand} alt="" />
       </Hand>
-
       <Main>
         <div>
           <Title>About Us</Title>
@@ -101,21 +128,23 @@ const About = () => {
         </div>
         <Content>
           <Rocket>
-            <img src={rocket} alt="Hand" width="400" height="400" />
+            <img src={rocket} alt="" width="400" height="400" />
           </Rocket>
-
           <AboutText>
             <Human>
-              <img src={human} alt="Hand" width="400" height="400" />
+              <img src={human} alt="" width="400" height="400" />
             </Human>
+
             <Text>
-              안녕하십니까 세상을 디자인 하는 개발자 여진호 입니다. <br />
-              방문해주셔서 감사합니다.
+              We help our customers to tell about themselves, to grow and stand
+              out in an increasingly competitive digital world, through creative
+              projects that are able to attract and involve, creating strategic
+              value.
             </Text>
             <div>
               <Circle style={{ backgroundColor: "var(--purple)" }} />
               <Circle style={{ backgroundColor: "var(--pink)" }} />
-              <Circle style={{ backgroundColor: "var(--black}" }} />
+              <Circle style={{ backgroundColor: "var(--black)" }} />
             </div>
           </AboutText>
         </Content>
